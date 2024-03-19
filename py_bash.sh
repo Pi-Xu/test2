91b8abaf038b8553c79907b23f05e16cec7425dd
suyan <wenqian.pi.x@gmail.com>
2024-03-19 12:47:33 +0800

    test second time

--- py_bash.sh
+++ py_bash.sh
@@ -2,4 +2,4 @@ python jem/jemdoc.py index
 python jem/jemdoc.py PI
 python jem/jemdoc.py publication
 python jem/jemdoc.py research
-python jem/jemdoc.py people
+python jem/jemdoc.py -c mysite.conf *.jemdoc
